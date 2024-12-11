6de6ca42948a3767c3f8e2af0527e82cbfdd5056
deepkontham <124440440+deepkontham@users.noreply.github.com>
2024-12-11 15:46:22 +0530

    Delete BankServices directory

--- BankServices/src/main/java/com/example/Brepository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Brepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Bank;
-
-public interface BankRepository extends JpaRepository<Bank, Long>{
-
-}
